8e9dee03d6322c6e34f480ef366f11abf2f58b8d
Andrii Liubimov <andrii.liubimov@gmail.com>
2022-08-31 19:50:48 -0400

    Nvim config

---
+++ lua/user/options.lua
@@ -0,0 +1,48 @@
+vim.opt.encoding = 'utf-8'
+vim.opt.termguicolors = true
+vim.opt.swapfile = false
+vim.opt.relativenumber = true
+vim.opt.number = true
+
+vim.opt.backup = false
+--vim.opt.undodir = '~/.vim/tmp/undo//'
+--vim.opt.backupdir = '~/.vim/tmp/backup//'
+--vim.opt.directory = '~/.vim/tmp/swap//'
+
+vim.opt.cursorline = false
+vim.opt.title = true
+vim.opt.autoindent = false
+vim.opt.ruler = true
+vim.opt.shortmess = 'aoOTI'
+vim.opt.showmode = true
+vim.opt.splitbelow = true
+vim.opt.splitright = true
+vim.opt.laststatus = 2
+vim.opt.modeline = false
+vim.opt.showcmd = true
+vim.opt.showmatch = true
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+vim.opt.cinoptions = '(0,m1,:1'
+vim.opt.formatoptions = 'tcqr2'
+vim.opt.laststatus = 2
+vim.opt.modeline = false
+vim.opt.clipboard = 'unnamed'
+vim.opt.softtabstop = 4
+vim.opt.showtabline = 1
+vim.opt.smartcase = true
+vim.opt.sidescroll = 5
+vim.opt.scrolloff = 4
+vim.opt.hlsearch = true
+vim.opt.ttyfast = true
+vim.opt.history = 10000
+vim.opt.hidden = true
+vim.opt.backspace = 'indent,eol,start'
+vim.opt.ttimeoutlen = 100
+vim.opt.updatetime = 500
+vim.opt.exrc = true
+
+vim.opt.complete = '.,w,b,u,t'
+vim.opt.completeopt = 'longest,menuone,preview'
+
